282a8d58eb70cfd89f588b7069d20eab96812017
pedroheck <pedroscheck@hotmail.com>
2021-02-25 17:19:35 -0300

    Fixing child position

--- Classes/Herbivoro.js
+++ Classes/Herbivoro.js
@@ -10,20 +10,19 @@ export class Herbivoro extends Organismo{
 
     // Método de reprodução (com mutações)
     reproduzir(){
-        var probabilidade_mutacao = 1; // chances de cada gene (atributo) sofrer mutação
+        var probabilidade_mutacao = 0.5; // chances de cada gene (atributo) sofrer mutação
         var sofreu_mutacao = false;
         // As mutações poderão ter valores positivos ou negativos com a mesma probabilidade (daí o "- 0.5").
         // A divisão é para diminuir a magnitude da diferença de uma geração para a outra
         // o (...).toFixed(4) é para arredondar para 4 casas decimais, mas retorna uma String
         // então o parseFloat() transforma tudo em float novamente
-
+        console.log("\n--------------- Mutações ocorridas ----------------\n")
         // raio
         var mutacao_raio = parseFloat(((Math.random() - 0.5) / 4).toFixed(4));
         var raio_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\nraio do pai: " + this._raio);
             raio_filho = parseFloat((this._raio + mutacao_raio).toFixed(4));
-            console.log("raio do filho: " + raio_filho);
+            console.log("\nRaio: " + this._raio + " --> " + raio_filho);
             sofreu_mutacao = true;
         }
         else{
@@ -34,9 +33,8 @@ export class Herbivoro extends Organismo{
         var mutacao_vel_max = parseFloat(((Math.random() - 0.5) / 2).toFixed(4));
         var vel_max_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\nvel máx do pai: " + this._vel_max);
             vel_max_filho = parseFloat((this._vel_max + mutacao_vel_max).toFixed(4));
-            console.log("vel máx filho: " + vel_max_filho);
+            console.log("Vel máx: " + this._vel_max + " --> " + vel_max_filho);
             sofreu_mutacao = true;
         } else{
             vel_max_filho = this._vel_max;
@@ -45,9 +43,8 @@ export class Herbivoro extends Organismo{
         var mutacao_forca_max = parseFloat(((Math.random() - 0.5) / 2).toFixed(4));
         var forca_max_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\nforça máx pai: " + this._forca_max);
             forca_max_filho = parseFloat((this._forca_max + mutacao_forca_max).toFixed(4));
-            console.log("força máx filho: " + forca_max_filho);
+            console.log("Força máx: " + this._forca_max + " --> " + forca_max_filho);
             sofreu_mutacao = true;
         } else{
             forca_max_filho = this._forca_max;
@@ -58,9 +55,8 @@ export class Herbivoro extends Organismo{
         var mutacao_raio_deteccao = parseFloat(((Math.random() - 0.5) / 2).toFixed(4));
         var raio_deteccao_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\nraio de detecção pai: " + this._raio_deteccao);
             raio_deteccao_filho = parseFloat((this._raio_deteccao + mutacao_raio_deteccao).toFixed(4));
-            console.log("raio de detecção filho: " + raio_deteccao_filho);
+            console.log("Raio de detecção: " + this._raio_deteccao + " --> " + raio_deteccao_filho);
             sofreu_mutacao = true;
         } else{
             raio_deteccao_filho = this._raio_deteccao;
@@ -70,9 +66,8 @@ export class Herbivoro extends Organismo{
         var mutacao_energia_max = parseFloat(((Math.random() - 0.5) / 2).toFixed(4));
         var energia_max_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\nenergia máx pai: " + this._energia_max);
             energia_max_filho = parseFloat((this._energia_max + mutacao_energia_max).toFixed(4));
-            console.log("energia máx filho: " + energia_max_filho);
+            console.log("Energia máx: " + this._energia_max + " --> " + energia_max_filho);
             sofreu_mutacao = true;
         } else{
             energia_max_filho = this._energia_max;
@@ -82,9 +77,8 @@ export class Herbivoro extends Organismo{
         var mutacao_taxa_gasto_energia = parseFloat(((Math.random() - 0.5) / 2).toFixed(4));
         var taxa_gasto_energia_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\ntaxa de gasto de energia pai: " + this._taxa_gasto_energia);
             taxa_gasto_energia_filho = parseFloat((this._taxa_gasto_energia + mutacao_taxa_gasto_energia).toFixed(4));
-            console.log("taxa de gasto de energia filho: " + taxa_gasto_energia_filho);
+            console.log("Taxa de gasto de energia: " + this._taxa_gasto_energia + " --> " + taxa_gasto_energia_filho);
             sofreu_mutacao = true;
         } else{
             taxa_gasto_energia_filho = this._taxa_gasto_energia;
@@ -94,9 +88,8 @@ export class Herbivoro extends Organismo{
         var mutacao_cansaco_maximo = parseFloat(((Math.random() - 0.5) / 2).toFixed(4));
         var cansaco_max_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\ncansaço máx pai: " + this._cansaco_max);
             cansaco_max_filho = parseFloat((this._cansaco_max + mutacao_cansaco_maximo).toFixed(4));
-            console.log("cansaço máx filho: " + cansaco_max_filho);
+            console.log("Cansaço máx: " + this._cansaco_max + " --> " + cansaco_max_filho);
             sofreu_mutacao = true;
         } else{
             cansaco_max_filho = this._cansaco_max;
@@ -106,20 +99,19 @@ export class Herbivoro extends Organismo{
         var mutacao_taxa_aum_cansaco = parseFloat(((Math.random() - 0.5) / 2).toFixed(4));
         var taxa_aum_cansaco_filho;
         if(Math.random() < probabilidade_mutacao){
-            console.log("\ntaxa de aumento do cansaço pai: " + this.taxa_aum_cansaco);
             taxa_aum_cansaco_filho = parseFloat((this._taxa_aum_cansaco + mutacao_taxa_aum_cansaco).toFixed(4));
-            console.log("taxa de aumento do cansaço filho: " + taxa_aum_cansaco_filho);
+            console.log("Taxa de aumento do cansaço: " + this._taxa_aum_cansaco + " --> " + taxa_aum_cansaco_filho);
             sofreu_mutacao = true;
         } else{
             taxa_aum_cansaco_filho = this._taxa_aum_cansaco;
         }
 
-        if(sofreu_mutacao == true){
-            console.log("Sofri uma mutação!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        }
+        // if(sofreu_mutacao == true){
+        //     console.log("Sofri uma mutação!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        // }
 
         return new Herbivoro(
-            this._x, this._y, raio_filho, this._vel, this._acel, vel_max_filho, forca_max_filho, 
+            this._posicao.x, this._posicao.y, raio_filho, this._vel, this._acel, vel_max_filho, forca_max_filho, 
             this._cor, raio_deteccao_filho, this._energia, energia_max_filho, taxa_gasto_energia_filho, 
             cansaco_max_filho, taxa_aum_cansaco_filho
             );
--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const herbivoro1 = new Herbivoro(100, 200, 7, 15, 6, 20, 3, "rosa", 8, 13, 18, 1
 const carnivoro1 = new Carnivoro(200,200,5,2,6,5,3,"verde",3,4,1,2,2,4);
 const alimento1 = new Alimento(100,100,4);
 
-console.log(herbivoro1);
+console.log("\n\n------------ Herbívoro -------------\n\n");
+console.log(herbivoro1)
 
 // testando criação de filho
 var clone_herbivoro = herbivoro1.reproduzir();
-console.log(clone_herbivoro)
+console.log("\n\n------------ Clone do herbívoro -------------\n\n");
+console.log(clone_herbivoro);
+
 
 // // testando criação de filhos dos filhos
 // var clone_do_clone = clone_herbivoro.reproduzir();
