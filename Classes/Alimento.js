fa03467f9ddd751305142a0d39097111dca82d77
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-06-26 17:53:56 -0300

    cleaning branch

--- Classes/Alimento.js
+++
@@ -1,9 +0,0 @@
-export class Alimento{
-    constructor(x, y, raio){
-        this.x = x;
-        this.y = y;
-        this.raio = raio;
-        // a energia do pedaço de alimento é proporcinal à sua área
-        this.qtd_energia = Math.floor(Math.PI * Math.pow(raio, 2))
-    }
-}
